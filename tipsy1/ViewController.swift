72bae1948263b57fff1c51c972811f1c8db57818
Denisov Denis <original163@mail.ru>
2021-05-26 21:58:27 +0400

    Initial Commit

---
+++ tipsy1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipsy1
+//
+//  Created by Денис Денисов on 26.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
