e91122dce55717e90ef78953cad9c19d69362484
Denisov Denis <original163@mail.ru>
2021-05-27 21:04:09 +0400

    Были создвны IBActions и IBOutlets для Calculator Screen

---
+++ tipsy1/CalculatorViewController.swift
@@ -0,0 +1,36 @@
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPicButton: UIButton!
+    @IBOutlet weak var tenPicButton: UIButton!
+    @IBOutlet weak var twentyPicButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChnged(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
